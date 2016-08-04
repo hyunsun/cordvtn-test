486911d60810406190d27d13d85848d34604af99
Hyunsun Moon <hyunsun.moon@gmail.com>
2016-08-04 17:25:21 -0400

    Added to create br-ex to mgmt-setup.sh

--- mgmt-setup.sh
+++ mgmt-setup.sh
@@ -2,6 +2,7 @@
 
 # Management
 sudo ovs-vsctl add-br br-mgmt
+sudo ovs-vsctl add-br br-ex
 sudo ovs-vsctl add-port br-ex vxlan-c1 -- set interface vxlan-c1 type=vxlan options:remote_ip=159.203.255.221 options:local_ip=45.55.25.244 options:key=100
 sudo ip link set br-mgmt up
 sudo ip addr add 10.10.10.21/24 dev br-mgmt
