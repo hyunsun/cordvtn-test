bafa97f5e7abcaa05e980041de052046e6780168
Hyunsun Moon <hyunsun.moon@gmail.com>
2017-01-01 20:21:37 -0500

    updated mgmt setup

--- mgmt-setup.sh
+++ mgmt-setup.sh
@@ -3,8 +3,6 @@
 # Management
 sudo ovs-vsctl add-br br-mgmt
 sudo ovs-vsctl add-port br-mgmt vxlan-c1 -- set interface vxlan-c1 type=vxlan options:remote_ip=107.170.228.96 options:local_ip=45.55.25.244 options:key=100
-sudo ovs-vsctl add-port br-mgmt vxlan-c2 -- set interface vxlan-c2 type=vxlan options:remote_ip=198.199.96.13 options:local_ip=45.55.25.244 options:key=100
-sudo ovs-vsctl add-port br-mgmt vxlan-c3 -- set interface vxlan-c3 type=vxlan options:remote_ip=162.243.151.127 options:local_ip=45.55.25.244 options:key=100
 sudo ip link set br-mgmt up
 sudo ip addr add 10.10.10.21/24 dev br-mgmt
 sudo ifconfig br-mgmt mtu 1400
