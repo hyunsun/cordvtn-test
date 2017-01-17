6fea345dd4932ca64c33db13ad2a473d203fc401
hyunsun <hyunsun.moon@gmail.com>
2017-01-17 10:44:20 +0000

    Fixed OLT container IP address

--- docker-olt/run-olt.sh
+++ docker-olt/run-olt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo docker run --privileged --cap-add=ALL -d -v /dev:/dev -v /lib/modules:/lib/modules --name olt -t vlan /bin/bash
-sudo ./pipework fabric -i eth1 olt 10.168.0.254/24
+sudo ./pipework fabric -i eth1 olt 10.6.1.254/24
 sudo docker exec -d olt modprobe 8021q
 sudo docker exec -d olt vconfig add eth1 222
 sudo docker exec -d olt ip link set eth1.222 up
-sudo docker exec -d olt ip addr add 10.169.0.254/24 dev eth1.222
+sudo docker exec -d olt ip addr add 10.168.0.254/24 dev eth1.222
